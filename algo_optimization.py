a8f5f874fd32247313f52c687b63ae0c2d1e9560
yaia <yaia1309@gmail.com>
2016-07-03 17:04:59 +0300

    writing final code

--- algo_optimization.py
+++ algo_optimization.py
@@ -1,1 +1,10 @@
-# need to write function to plot error plot and one to plot the track
+# Draw results
+DEBUG = True
+
+# route table
+# split to different tracks in order to not overfit while testing
+ROUTE_TABLE = [[0, 0, 1, 1], [2, 2, 3, 3, 4, 4], [5, 6], [7, 7, 8, 8, 9]]
+# Tested route
+ROUTE_TEST = 9
+
+# Write to file. None for no file written
